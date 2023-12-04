c55a04bc61463846a11108e538287a5b8ba81cb7
Hououin Kyouma <inwazzin@gmail.com>
2023-12-04 21:45:42 +0100

    lock

--- src/application.tsx
+++ src/application.tsx
@@ -1,5 +1,5 @@
-import { render } from "solid-js/web";
 import "./styles.scss";
+import { render } from "solid-js/web";
 import { GameWorld } from "@modules/world/GameWorld.js";
 import { GameInterface } from "@modules/interface/GameInterface.js";
 import { DevelopmentTools } from "@modules/development/DevelopmentTools.js";
